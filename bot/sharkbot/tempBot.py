7cc2db6bddc381a4c48303d88c86edd18cb41cc7
kushte <kmartinmburu@gmail.com>
2021-06-03 21:02:32 +0300

    Multithreaded bot version --  No clean exit

--- bot/sharkbot/tempBot.py
+++ bot/sharkbot/tempBot.py
@@ -16,13 +16,14 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
 from selenium.common.exceptions import ElementNotVisibleException 
 
 
-class SharkBot(object):
+class SharkBotTemp(object):
     def __init__(self, user_details, settings, logger = None):
         # Set up
         self.order_list = set()
         self.email = user_details['email']
         self.password = user_details['password']
         self.logger = logger
+        self.logger.info("Initializing browser instance")
         self.counter = 0
         # message list
         self.message_list = settings['messages']
@@ -99,6 +100,7 @@ class SharkBot(object):
             print("Press Ctrl-C to exit")
             self.logger.info("Exiting program...")
             self.stop_bot()
+    
 
         
 
@@ -118,6 +120,66 @@ class SharkBot(object):
             # return empty set
             order_urls= ()
         return order_urls
+
+    def process_queued_orders(self,queue, event):
+        # check for url in queue when queue is not empty
+        while not event.is_set() or not queue.empty():
+            # get a url and start the bidding process
+            order = queue.get()
+            self.logger.info("Consumer Bidding bot applyin for url: %s (size=%d)", order, queue.qsize())
+            #### fetch url
+            self.driver.get(order)
+            # get time to wait
+            try:
+                WebDriverWait(self.driver, 45).until(EC.visibility_of_any_elements_located((By.ID, "id_read_timeout_container")))
+                time_to_wait_element = self.driver.find_element_by_id("id_read_timeout_sec")
+                if (time_to_wait_element.is_displayed()):
+                    time_to_wait = int(time_to_wait_element.text)
+                    self.logger.info("Sleeping for %s secs", time_to_wait)
+                    time.sleep(time_to_wait)
+                # check if file download is required
+                file_check= self.driver.find_element_by_id("id_read_files_msg_container").is_displayed()
+                if file_check:
+                    self.download_files()
+
+            except (ElementNotVisibleException, NoSuchElementException, TimeoutException) as e:
+                self.logger.debug("Unable to find waiting order block")
+           # order_url_element = self.driver.find_elements_by_css_selector('td.order_number>a')
+            # Bid for the order
+
+            
+            ##process url
+            ### get recommended bid amount
+            try:
+                WebDriverWait(self.driver, 240).until(EC.visibility_of_any_elements_located((By.ID, "rec_amount")))
+                recommended_bid = self.driver.find_element_by_id("rec_amount").text
+                # enter amount in bid amount
+                bid_field = self.driver.find_element_by_id("id_bid")
+                bid_field.send_keys(recommended_bid)
+            except (ElementNotVisibleException, NoSuchElementException, TimeoutException) as e:
+                self.logger.info("Recommeded amount not found")
+            else:
+                """Only runs if recommended amount present"""
+                # click apply button
+                if (self.BID_STATUS == "True"):
+                    try:
+                        WebDriverWait(self.driver, 240).until( EC.element_to_be_clickable((By.ID, "apply_order")))
+                        apply_button = self.driver.find_element_by_id("apply_order")
+                        apply_button.click()
+                        self.counter = self.counter + 1
+                        self.logger.info("Applied for Order [%s]", order)
+                        self.logger.info("%s orders have been processed", self.counter)
+                    except (ElementNotVisibleException, NoSuchElementException, TimeoutException) as e:
+                        self.logger.info("Apply button not found")
+                    else:
+                        self.chat_client()
+                                       
+                else:
+                    self.logger.info("Bid Status set to false. Order not applied")
+                    self.counter = self.counter + 1
+                    self.logger.info("%s orders have been processed", self.counter)
+
+
         
     def process_orders(self,queued_orders):
         """Bid for order and talk to customer"""
@@ -223,8 +285,25 @@ class SharkBot(object):
         #navigate to home page  
         self.logger.info("Navigating to home page")
         self.driver.get("https://essayshark.com/")
+        # Log in via thread
+        self.logger.info("Attempting to log in")
         self.load_popup()
+        
         self.enter_user_details()
+        # set up loop to keep browser window open indefinetly
+        """
+        try:
+            while True:
+               print("Waiting for input from Queue")
+               #time.sleep(random.randrange(self.DELAY, 25))
+                    
+        except KeyboardInterrupt:
+            print("Press Ctrl-C to exit")
+            self.logger.info("Exiting program...")
+            self.stop_bot()"""
+        """
+        self.driver.get("https://essayshark.com/")
+
         # get existing orders
         fetched_orders = self.fetch_orders()
         self.logger.debug(fetched_orders)
@@ -232,12 +311,12 @@ class SharkBot(object):
         # get new orders
         self.order_list.update(fetched_orders)
         # wait for new orders
-        self.wait_for_new_orders()
+        self.wait_for_new_orders()"""
 
 
 
     def stop_bot(self):
         """Exit and clean up function"""
-        self.logger.debug("Calling exit function")
+        self.logger.debug("Browser window exiting")
         self.driver.quit()
-        sys.exit(2)
+        sys.exit()
--- bot/tempMain.py
+++ bot/tempMain.py
@@ -5,11 +5,18 @@ import sys
 import logging
 import logging.config
 import json
+import queue
+import threading
+import time
+import concurrent.futures
 
 from configparser import ConfigParser
 from datetime import date, datetime, timedelta
-from sharkbot.tempBot import SharkBot
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, WebDriverException
+from sharkbot.tempBot import SharkBotTemp
 
+
+        
 def start():
     # set up logger
     logger = setup_logger()
@@ -20,16 +27,110 @@ def start():
     settings = fetch_settings(logger)
     sell_date = '2021-05-13'
     sell_date = datetime.strptime(sell_date, '%Y-%m-%d')
-    expiry_date = sell_date + timedelta(20)
+    expiry_date = sell_date + timedelta(200)
     if (expiry_date > datetime.now()):
         difference = expiry_date - datetime.now()
         logger.info("Executing trial version. You have %s days remaining.", difference.days)
-        bot = SharkBot(user_details,settings, logger)
-        bot.start_bot()
+        initialize_bots(user_details,settings, logger)
+        
+        """
+
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
+        executor.submit(bot_1.start_bot) 
+        executor.submit(bot_2.start_bot)
+
+        # create producer consumer bots
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            executor.submit(main_bot.start_bot)
+            executor.submit(bot_1.start_bot)
+            executor.submit(bot_2.start_bot)
+        """
+
 
     else:
         logger.info("Your trial period has expired")
 
+
+
+
+def initialize_bots(user_details,settings, logger):
+    """Function to initialize and run bot"""
+    # Initialize producer bot
+    main_bot = SharkBotTemp(user_details,settings, logger)
+    # initalize multple worker bots
+    bot_1 = SharkBotTemp(user_details,settings, logger)
+    bot_2 = SharkBotTemp(user_details,settings, logger)
+    # log in for all bots
+    main_bot.start_bot()
+    bot_1.start_bot()
+    bot_2.start_bot()
+    pipeline = queue.Queue(maxsize=10)
+    event = threading.Event()
+    
+
+    #executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+
+
+    # store existing orders during bot start up
+    fetched_orders = main_bot.fetch_orders()
+    print("Initial orders found on Ac")
+    print(fetched_orders)
+    main_bot.order_list.update(fetched_orders)
+
+    # start bidding bots in seperate threads
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+            executor.submit(bot_1.process_queued_orders, pipeline, event)
+            executor.submit(bot_2.process_queued_orders, pipeline, event)
+
+
+            # fetch new orders using main thread
+            ## to do set up loop to check for new orders
+            while True:
+                try:                
+                    new_orders = main_bot.fetch_orders()
+                    queued_orders = new_orders.difference(main_bot.order_list)
+                    if len(queued_orders) > 0:
+                        # put urls in queue for children bidding bots
+                        print(queued_orders)
+                        for order in queued_orders:
+                            ## add order to queue for processing
+                            pipeline.put(order)
+                            # add to list of processed orders
+                            main_bot.order_list.add(order)
+                        main_bot.driver.get("https://essayshark.com/writer/orders/")
+                    else:
+                        logger.info("No new orders to bid found")
+                        time.sleep(main_bot.randomize_delay())
+                except (KeyboardInterrupt, WebDriverException, TimeoutException):
+                    print("Exiting after pressing Ctrl-C to exit")
+                    logger.info("Starting the Exiting process...")
+                    # signal bots to stop running
+                    event.set()
+                    #logger.info("Event to shutdown threads emitted")
+                    # stop threads
+                    #executor.shutdown(wait=True)
+                    #logger.info("All threads shut down")
+
+                    # close bots                
+                    main_bot.stop_bot()
+                    # loop to close children bots
+                    bot_1.stop_bot()
+                    bot_2.stop_bot()
+                
+
+
+            # close bots                
+            main_bot.stop_bot()
+            # loop to close children bots
+            bot_1.stop_bot()
+            bot_2.stop_bot()
+
+
+
+
+
+        #queue.put(queued_orders)
 
 
 
