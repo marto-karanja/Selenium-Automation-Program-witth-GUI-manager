358d09246be94aa608d799a006f6ce9229f67022
kushte <kmartinmburu@gmail.com>
2021-06-04 22:02:24 +0300

    Adding launcher file to load config details

---
+++ bot/bot_launcher.py
@@ -0,0 +1,114 @@
+# fetch chrome driver and settings
+import json
+import os
+import sys
+import logging
+import logging.config
+import json
+import queue
+import threading
+import time
+import concurrent.futures
+
+from configparser import ConfigParser
+from datetime import date, datetime, timedelta
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, WebDriverException
+
+
+
+class Launcher():
+    """Initializes configuration settings to run the bot"""
+
+    def __init__(self, logger = None):
+        """Initialize class and set up defaults"""
+        # create logger object
+        if logger is None:
+            logger = self.setup_logger()
+        self.logger = logger
+        self.bot_settings = self.fetch_settings()
+        self.user_details = self.fetch_user_details()
+    
+    def setup_logger(self):
+        """function to set up logger"""
+        # configure logging
+        d = date.today()
+        log_file = d.isoformat()
+        ## check if file exists
+        log_path = self.check_log_folder('logs')
+        logging.basicConfig(format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s", datefmt="'%m/%d/%Y %I:%M:%S %p",
+        handlers=[
+            logging.FileHandler("{0}/{1}.log".format(log_path, log_file)),
+            logging.StreamHandler()],
+            level = logging.INFO)
+        logger = logging.getLogger(__name__)
+        return logger
+    
+
+    def check_log_folder(self,dir_name):
+        """Function to ensure log directory exists"""
+        if not os.path.exists(dir_name):
+            try:
+                os.mkdir(dir_name)
+            except OSError:
+                print ("[INFO] Creation of the directory {} failed".format(os.path.abspath(dir_name)))
+        else:
+            print ("[INFO] Successfully created the directory {} ".format(os.path.abspath(dir_name)))
+        return format(os.path.abspath(dir_name))
+
+    
+
+    def resource_path(self, relative_path):
+        """ returns correct path to chrome driver """
+
+        try:
+            base_path = sys._MEIPASS
+            
+        except Exception:
+            base_path = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(base_path, relative_path)
+
+    def fetch_settings(self):
+        """Fetch to fetch configuration settings from setup.ini"""
+        """Fetch Path to Chrome driver from config file"""
+        """
+        Returns settings dictionary of the form 
+        settings
+        ['delay':int,
+        'chromedriver': string,
+        'messages': [string],
+        'bid_status'[Boolean],]
+        """
+        config = ConfigParser()
+        settings = {}
+        config.read("setup.ini")
+        CHROME_DRIVER_PATH = config.get("chromedriver", "path")
+        bid_status = config.get("bid_status","bid")
+        # Fetch delay
+        settings['delay']= config.getint("delay", "seconds")
+        chrome_driver = self.resource_path(CHROME_DRIVER_PATH)
+        self.logger.debug("Successfully fetched chrome driver path")
+        # fetch client messages
+        messages = json.loads(config.get("client_message","messages"))
+        settings['messages'] = messages
+        self.logger.info("Successfully fetched client messages")
+        settings['chromedriver'] = chrome_driver
+        settings['bid_status'] = bid_status
+        return settings
+
+    def fetch_user_details(self):
+        """Function to fetch user details from config.json"""
+        with open('config.json', 'r') as f:
+            config = json.load(f)
+        
+        user_details= {}
+        user_details['email'] = config['user_details']['email']
+        user_details['password'] = config['user_details']['password']
+        self.logger.debug("User details fetched successfully")
+        return user_details
+
+    def check_expiry(self):
+        """checks if trial license is valid, returns expiry date"""
+        sell_date = '2021-05-13'
+        sell_date = datetime.strptime(sell_date, '%Y-%m-%d')
+        expiry_date = sell_date + timedelta(200)
+        return expiry_date
--- bot/main_gui.py
+++ bot/main_gui.py
@@ -4,6 +4,10 @@ from tkinter import scrolledtext
 from tkinter import Menu
 from tkinter import messagebox as msg
 from tkinter import Spinbox
+from datetime import date, datetime, timedelta
+from sharkbot.tempBot import SharkBotTemp
+
+from bot_launcher import Launcher
 
 class SharkGui():
     def __init__(self):         # Initializer method
@@ -16,4 +20,141 @@ class SharkGui():
 
         # Change the main windows icon
         self.win.iconbitmap('gui/assets/sharkbot.ico')      
+
+        # Launch bot settings
+        self.launcher = Launcher()
+        self.logger = self.launcher.logger
+        self.logger.info("Logger and configuration details successfully set")
+
+
+        # draw widgets
         self.create_widgets()
+
+    # Button to start Bot execution
+    def start_bot(self): 
+        self.bot_button.configure(text='Starting  ' + self.number_of_bots_chosen.get() + ' Bidders')
+        # check for expiry
+        expiry_date = self.launcher.check_expiry()
+        if (expiry_date > datetime.now()):
+            difference = expiry_date - datetime.now()
+            self.logger.info("Executing trial version. You have %s days remaining.", difference.days)
+            self.initialize_bots()
+        else:
+            msg.showerror('SharkBot Message Error Box', 'Your trial period has expired.') 
+
+    def initialize_bots(self):
+        """Function to initialize and run bot"""
+        # Initialize producer bot
+        main_bot = SharkBotTemp(user_details,settings, logger)
+        # initalize multple worker bots
+        bot_1 = SharkBotTemp(user_details,settings, logger)
+        bot_2 = SharkBotTemp(user_details,settings, logger)
+        # log in for all bots
+        main_bot.start_bot()
+        bot_1.start_bot()
+        bot_2.start_bot()
+
+
+        # Spinbox callback 
+    def _spin(self):
+        value = self.bot_delay_spin.get()
+        print(value)
+        self.scrol.insert(tk.INSERT, value + '\n')
+
+
+
+
+        # Exit GUI cleanly
+    def _quit(self):
+        self.win.quit()
+        self.win.destroy()
+        exit() 
+                  
+    #####################################################################################       
+    def create_widgets(self):    
+        tabControl = ttk.Notebook(self.win)          # Create Tab Control
+        
+        tab1 = ttk.Frame(tabControl)            # Create a tab 
+        tabControl.add(tab1, text='Tab 1')      # Add the tab
+        tab2 = ttk.Frame(tabControl)            # Add a second tab
+        tabControl.add(tab2, text='Tab 2')      # Make second tab visible
+        
+        tabControl.pack(expand=1, fill="both")  # Pack to make visible
+        
+        # LabelFrame using tab1 as the parent
+        main_window = ttk.LabelFrame(tab1, text='SharkBot Settings')
+        main_window.grid(column=0, row=0, padx=8, pady=4)
+        
+        # Modify adding a Label using mighty as the parent instead of win
+        a_label = ttk.Label(main_window, text="Choose Bot Delay:")
+        a_label.grid(column=0, row=0, sticky='W')
+    
+    
+        # Adding a Spinbox widget
+        self.bot_delay_spin = Spinbox(main_window, values=(2, 4, 6, 8, 10, 12, 15,20, 25, 30, 60, 120, 240), width=5, bd=9, command=self._spin)# using range
+        self.bot_delay_spin.grid(column=0, row=1)
+                      
+        
+        # Adding a Button
+        self.bot_button = ttk.Button(main_window, text="Start Bot!", command=self.start_bot)   
+        self.bot_button.grid(column=2, row=1)                                
+        
+        ttk.Label(main_window, text="Choose number of bidders:").grid(column=1, row=0)
+        self.number_of_bots = tk.StringVar()
+        self.number_of_bots_chosen = ttk.Combobox(main_window, width=12, state='readonly', textvariable=self.number_of_bots)
+        self.number_of_bots_chosen['values'] = (1, 2, 3, 4, 5, 6)
+        self.number_of_bots_chosen.grid(column=1, row=1)
+        self.number_of_bots_chosen.current(2)
+        
+             
+
+        # Using a scrolled Text control    
+        scrol_w  = 30
+        scrol_h  =  3
+        self.scrol = scrolledtext.ScrolledText(main_window, width=scrol_w, height=scrol_h, wrap=tk.WORD)
+        self.scrol.grid(column=0, row=3, sticky='WE', columnspan=3)                    
+        
+        
+        # Tab Control 2 ----------------------------------------------------------------------
+        # We are creating a container frame to hold all other widgets -- Tab2
+
+            
+        # Creating a Menu Bar
+        menu_bar = Menu(self.win)
+        self.win.config(menu=menu_bar)
+        
+        # Add menu items
+        file_menu = Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="New")
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=self._quit)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+        
+        # Display a Message Box
+        def _msgBox():
+            msg.showinfo('SharkBot Message Info Box', 'SharkBot Bidder: Apply for multiple orders at the best speed possible.')  
+            
+        # Add another Menu to the Menu Bar and an item
+        help_menu = Menu(menu_bar, tearoff=0)
+        help_menu.add_command(label="About", command=_msgBox)   # display messagebox when clicked
+        menu_bar.add_cascade(label="Help", menu=help_menu)
+
+        #########################################################################
+        # display settings
+        self.scrol.insert(tk.INSERT, self.launcher.bot_settings)
+
+
+
+        
+
+
+        
+
+        
+   
+         
+#======================
+# Start GUI
+#======================
+sharkgui = SharkGui()
+sharkgui.win.mainloop()
